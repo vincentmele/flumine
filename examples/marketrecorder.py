ed50ddf6b2efbac37d8866fe43ef8938cc865845
Vincent Mele <vincentmele@gmail.com>
2020-06-13 23:07:42 +0100

    use dynaconf for configuration

--- examples/marketrecorder.py
+++ examples/marketrecorder.py
@@ -3,7 +3,7 @@ import logging
 import betfairlightweight
 from pythonjsonlogger import jsonlogger
 
-import configparser
+from dynaconf import settings, Validator
 
 from flumine import Flumine, clients
 from flumine.streams.datastream import DataStream
@@ -11,8 +11,6 @@ from strategies.marketrecorder import MarketRecorder
 
 logger = logging.getLogger()
 
-config = configparser.SafeConfigParser()
-config.read('config')
 
 custom_format = "%(asctime) %(levelname) %(message)"
 log_handler = logging.StreamHandler()
@@ -22,8 +20,15 @@ log_handler.setFormatter(formatter)
 logger.addHandler(log_handler)
 logger.setLevel(logging.INFO)
 
-trading = betfairlightweight.APIClient(config.get('betfair', 'username'), config.get('betfair', 'password'),
-                                       config.get('betfair', 'app_key'), config.get('betfair', 'certs'))
+settings.validators.register(
+    Validator('USERNAME', 'PASSWORD', 'APP_KEY', 'CERTS', 'EVENT_TYPE_IDS', 'MARKET_TYPES', must_exist = True),
+    Validator('EVENT_TYPE_IDS', 'MARKET_TYPES', is_type_of=list)
+
+)
+settings.validators.validate()
+
+trading = betfairlightweight.APIClient(settings.USERNAME, settings.PASSWORD,
+                                       settings.APP_KEY, settings.CERTS)
 client = clients.BetfairClient(trading)
 
 framework = Flumine(client=client)
@@ -31,10 +36,14 @@ framework = Flumine(client=client)
 strategy = MarketRecorder(
     name="WIN",
     market_filter=betfairlightweight.filters.streaming_market_filter(
-        event_type_ids=["7"], country_codes=["GB", "IE"], market_types=["WIN"],
+        event_type_ids=settings.EVENT_TYPE_IDS,
+        country_codes=settings.get('COUNTRY_CODES', None),
+        market_types=settings.MARKET_TYPES,
     ),
     stream_class=DataStream,
-    context={"local_dir": "/tmp", "force_update": False, "remove_file": True,},
+    context={"local_dir": settings.LOCAL_DIR,
+             "force_update": settings.as_bool('FORCE_UPDATE'),
+             "remove_file": settings.as_bool('REMOVE_FILE')},
 )
 
 framework.add_strategy(strategy)
